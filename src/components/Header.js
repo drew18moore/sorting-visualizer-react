f444d653af741ef90151ef1d0d4533cbbb1c354f
Drew <dcm752052@gmail.com>
2022-10-05 22:45:14 -0400

    Created Header component and Header css file. Header currently contains nav with just an h1 logo

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import '../Header.css';
+
+function Header() {
+    return (
+        <nav>
+            <div id='nav-logo'>
+                <h1>sorting-visualizer</h1>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;

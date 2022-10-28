6f7c227cca1fd55e22b1971de4ce952eec2c7e7f
Drew <dcm752052@gmail.com>
2022-10-28 18:09:22 -0400

    Implemented merge sort (not animated).

--- src/components/Main.js
+++ src/components/Main.js
@@ -40,6 +40,19 @@ function Main(props) {
                 break;
             case "Merge Sort":
                 console.log("Performing Merge Sort");
+                // Test
+                let test = [...data.values].sort((a, b) => a - b);
+                let result = mergeSort(data.values);
+                console.log(arraysAreEqual(test, result));
+
+                // TODO: properly animate merge sort
+                setData((prevData) => {
+                    return {
+                        ...prevData,
+                        values: result
+                    }
+                })
+                
                 break;
                         
         }
@@ -95,6 +108,31 @@ function Main(props) {
         }
         // Test
         await console.log(arraysAreEqual(arr, test));
+    }
+
+    const mergeSort = (arr) => {
+        if (arr.length <= 1)
+            return arr;
+
+        let mid = Math.floor(arr.length / 2)
+        let leftArr = arr.slice(0, mid);
+        let rightArr = arr.slice(mid, arr.length);
+
+        return merge(mergeSort(leftArr), mergeSort(rightArr));
+    }
+
+    const merge = (leftArr, rightArr) => {
+        let result = [];
+
+        while (leftArr.length && rightArr.length) {
+            if (leftArr[0] < rightArr[0]) {
+                result.push(leftArr.shift());
+            } else {
+                result.push(rightArr.shift());
+            }
+        }
+
+        return [...result, ...leftArr, ...rightArr]
     }
 
     const arraysAreEqual = (a, b) => {

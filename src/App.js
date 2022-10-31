d02196f8290b7aa67e42c4349bb827588dd45fb2
Drew <dcm752052@gmail.com>
2022-10-31 18:05:53 -0400

    Moved state from Main.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,152 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header'
 import Main from './components/Main'
 
 function App() {
   const [chosenAlgo, setChosenAlgo] = useState("");
+  const [data, setData] = useState({
+    num: 100,
+    values: []
+  })
+
+  useEffect(() => {
+    randomize();
+  }, [])
+
+  const randomize = () => {
+    setData(prevData => {
+        return {
+            ...prevData,
+            values: [...new Array(data.num)].map(() => Math.floor(Math.random() * 100) + 1)
+        }
+    });
+  }
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  const sort = () => {
+      switch(chosenAlgo) {
+          case "Bubble Sort":
+              console.log("Performing Bubble Sort");
+              bubbleSort();
+              break;
+          case "Selection Sort":
+              console.log("Performing Selection Sort");
+              selectionSort();
+              break;
+          case "Quick Sort":
+              console.log("Performing Quick Sort");
+              break;
+          case "Merge Sort":
+              console.log("Performing Merge Sort");
+              // Test
+              let test = [...data.values].sort((a, b) => a - b);
+              let result = mergeSort(data.values);
+              console.log(arraysAreEqual(test, result));
+
+              // TODO: properly animate merge sort
+              setData((prevData) => {
+                  return {
+                      ...prevData,
+                      values: result
+                  }
+              })
+              
+              break;
+                      
+      }
+  }
+
+  const bubbleSort = async() => {
+      let arr = [...data.values];
+      let test = [...arr].sort((a, b) => a - b);
+      let temp;
+      
+      for (let i = 0; i < arr.length; i++) {
+          for (let j = 0; j < arr.length - i - 1; j++) {
+              if (arr[j] > arr[j+1]) {
+                  temp = arr[j]
+                  arr[j] = arr[j+1]
+                  arr[j+1] = temp
+                  setData((prevData) => {
+                      return {
+                          ...prevData,
+                          values: arr
+                      }
+                  })
+                  await sleep(10);
+              } 
+          }
+      }
+      await console.log(arraysAreEqual(arr, test));
+  }
+
+  const selectionSort = async() => {
+      let arr = [...data.values];
+      let test = [...arr].sort((a, b) => a - b);
+      let temp;
+      let minIndex;
+
+      for (let i = 0; i < arr.length; i++) {
+          minIndex = i;
+          for (let j = i; j < arr.length; j++) {
+              if (arr[j] < arr[minIndex]) {
+                  minIndex = j;
+              }
+          }
+          temp = arr[i];
+          arr[i] = arr[minIndex];
+          arr[minIndex] = temp;
+          setData((prevData) => {
+              return {
+                  ...prevData,
+                  values: arr
+              }
+          })
+          await sleep(10);
+      }
+      // Test
+      await console.log(arraysAreEqual(arr, test));
+  }
+
+  const mergeSort = (arr) => {
+      if (arr.length <= 1)
+          return arr;
+
+      let mid = Math.floor(arr.length / 2)
+      let leftArr = arr.slice(0, mid);
+      let rightArr = arr.slice(mid, arr.length);
+
+      return merge(mergeSort(leftArr), mergeSort(rightArr));
+  }
+
+  const merge = (leftArr, rightArr) => {
+      let result = [];
+
+      while (leftArr.length && rightArr.length) {
+          if (leftArr[0] < rightArr[0]) {
+              result.push(leftArr.shift());
+          } else {
+              result.push(rightArr.shift());
+          }
+      }
+
+      return [...result, ...leftArr, ...rightArr]
+  }
+
+  const arraysAreEqual = (a, b) => {
+      if (a === b) return true;
+      if (a == null || b == null) return false;
+      if (a.length !== b.length) return false;
+
+      for (var i = 0; i < a.length; ++i) {
+          if (a[i] !== b[i]) return false;
+      }
+      return true;
+  }
 
   const handleChange = event => {
     setChosenAlgo(event.value);
@@ -13,7 +155,7 @@ function App() {
   return (
     <div className="App">
       <Header algo={chosenAlgo} onChange={handleChange}/>
-      <Main algo={chosenAlgo}/>
+      <Main vals={data.values} randomize={randomize} sort={sort}/>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,150 +3,150 @@ import '../Main.css'
 import Bar from './Bar';
 
 function Main(props) {
-    const [data, setData] = useState({
-        num: 100,
-        values: [12, 4, 59, 47, 94, 17]
-    });
+    // const [data, setData] = useState({
+    //     num: 100,
+    //     values: [12, 4, 59, 47, 94, 17]
+    // });
 
-    useEffect(() => {
-        randomize();
-    }, [])
+    // useEffect(() => {
+    //     randomize();
+    // }, [])
 
-    const randomize = () => {
-        setData(prevData => {
-            return {
-                ...prevData,
-                values: [...new Array(data.num)].map(() => Math.floor(Math.random() * 100) + 1)
-            }
-        });
-    }
+    // const randomize = () => {
+    //     setData(prevData => {
+    //         return {
+    //             ...prevData,
+    //             values: [...new Array(data.num)].map(() => Math.floor(Math.random() * 100) + 1)
+    //         }
+    //     });
+    // }
 
-    const sleep = (ms) => {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-    }
+    // const sleep = (ms) => {
+    //     return new Promise((resolve) => setTimeout(resolve, ms));
+    // }
 
-    const sort = () => {
-        switch(props.algo) {
-            case "Bubble Sort":
-                console.log("Performing Bubble Sort");
-                bubbleSort();
-                break;
-            case "Selection Sort":
-                console.log("Performing Selection Sort");
-                selectionSort();
-                break;
-            case "Quick Sort":
-                console.log("Performing Quick Sort");
-                break;
-            case "Merge Sort":
-                console.log("Performing Merge Sort");
-                // Test
-                let test = [...data.values].sort((a, b) => a - b);
-                let result = mergeSort(data.values);
-                console.log(arraysAreEqual(test, result));
+    // const sort = () => {
+    //     switch(props.algo) {
+    //         case "Bubble Sort":
+    //             console.log("Performing Bubble Sort");
+    //             bubbleSort();
+    //             break;
+    //         case "Selection Sort":
+    //             console.log("Performing Selection Sort");
+    //             selectionSort();
+    //             break;
+    //         case "Quick Sort":
+    //             console.log("Performing Quick Sort");
+    //             break;
+    //         case "Merge Sort":
+    //             console.log("Performing Merge Sort");
+    //             // Test
+    //             let test = [...data.values].sort((a, b) => a - b);
+    //             let result = mergeSort(data.values);
+    //             console.log(arraysAreEqual(test, result));
 
-                // TODO: properly animate merge sort
-                setData((prevData) => {
-                    return {
-                        ...prevData,
-                        values: result
-                    }
-                })
+    //             // TODO: properly animate merge sort
+    //             setData((prevData) => {
+    //                 return {
+    //                     ...prevData,
+    //                     values: result
+    //                 }
+    //             })
                 
-                break;
+    //             break;
                         
-        }
-    }
+    //     }
+    // }
 
-    const bubbleSort = async() => {
-        let arr = [...data.values];
-        let test = [...arr].sort((a, b) => a - b);
-        let temp;
+    // const bubbleSort = async() => {
+    //     let arr = [...data.values];
+    //     let test = [...arr].sort((a, b) => a - b);
+    //     let temp;
         
-        for (let i = 0; i < arr.length; i++) {
-            for (let j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j+1]) {
-                    temp = arr[j]
-                    arr[j] = arr[j+1]
-                    arr[j+1] = temp
-                    setData((prevData) => {
-                        return {
-                            ...prevData,
-                            values: arr
-                        }
-                    })
-                    await sleep(10);
-                } 
-            }
-        }
-        await console.log(arraysAreEqual(arr, test));
-    }
+    //     for (let i = 0; i < arr.length; i++) {
+    //         for (let j = 0; j < arr.length - i - 1; j++) {
+    //             if (arr[j] > arr[j+1]) {
+    //                 temp = arr[j]
+    //                 arr[j] = arr[j+1]
+    //                 arr[j+1] = temp
+    //                 setData((prevData) => {
+    //                     return {
+    //                         ...prevData,
+    //                         values: arr
+    //                     }
+    //                 })
+    //                 await sleep(10);
+    //             } 
+    //         }
+    //     }
+    //     await console.log(arraysAreEqual(arr, test));
+    // }
 
-    const selectionSort = async() => {
-        let arr = [...data.values];
-        let test = [...arr].sort((a, b) => a - b);
-        let temp;
-        let minIndex;
+    // const selectionSort = async() => {
+    //     let arr = [...data.values];
+    //     let test = [...arr].sort((a, b) => a - b);
+    //     let temp;
+    //     let minIndex;
 
-        for (let i = 0; i < arr.length; i++) {
-            minIndex = i;
-            for (let j = i; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-            setData((prevData) => {
-                return {
-                    ...prevData,
-                    values: arr
-                }
-            })
-            await sleep(10);
-        }
-        // Test
-        await console.log(arraysAreEqual(arr, test));
-    }
+    //     for (let i = 0; i < arr.length; i++) {
+    //         minIndex = i;
+    //         for (let j = i; j < arr.length; j++) {
+    //             if (arr[j] < arr[minIndex]) {
+    //                 minIndex = j;
+    //             }
+    //         }
+    //         temp = arr[i];
+    //         arr[i] = arr[minIndex];
+    //         arr[minIndex] = temp;
+    //         setData((prevData) => {
+    //             return {
+    //                 ...prevData,
+    //                 values: arr
+    //             }
+    //         })
+    //         await sleep(10);
+    //     }
+    //     // Test
+    //     await console.log(arraysAreEqual(arr, test));
+    // }
 
-    const mergeSort = (arr) => {
-        if (arr.length <= 1)
-            return arr;
+    // const mergeSort = (arr) => {
+    //     if (arr.length <= 1)
+    //         return arr;
 
-        let mid = Math.floor(arr.length / 2)
-        let leftArr = arr.slice(0, mid);
-        let rightArr = arr.slice(mid, arr.length);
+    //     let mid = Math.floor(arr.length / 2)
+    //     let leftArr = arr.slice(0, mid);
+    //     let rightArr = arr.slice(mid, arr.length);
 
-        return merge(mergeSort(leftArr), mergeSort(rightArr));
-    }
+    //     return merge(mergeSort(leftArr), mergeSort(rightArr));
+    // }
 
-    const merge = (leftArr, rightArr) => {
-        let result = [];
+    // const merge = (leftArr, rightArr) => {
+    //     let result = [];
 
-        while (leftArr.length && rightArr.length) {
-            if (leftArr[0] < rightArr[0]) {
-                result.push(leftArr.shift());
-            } else {
-                result.push(rightArr.shift());
-            }
-        }
+    //     while (leftArr.length && rightArr.length) {
+    //         if (leftArr[0] < rightArr[0]) {
+    //             result.push(leftArr.shift());
+    //         } else {
+    //             result.push(rightArr.shift());
+    //         }
+    //     }
 
-        return [...result, ...leftArr, ...rightArr]
-    }
+    //     return [...result, ...leftArr, ...rightArr]
+    // }
 
-    const arraysAreEqual = (a, b) => {
-        if (a === b) return true;
-        if (a == null || b == null) return false;
-        if (a.length !== b.length) return false;
+    // const arraysAreEqual = (a, b) => {
+    //     if (a === b) return true;
+    //     if (a == null || b == null) return false;
+    //     if (a.length !== b.length) return false;
 
-        for (var i = 0; i < a.length; ++i) {
-            if (a[i] !== b[i]) return false;
-        }
-        return true;
-    }
+    //     for (var i = 0; i < a.length; ++i) {
+    //         if (a[i] !== b[i]) return false;
+    //     }
+    //     return true;
+    // }
 
-    const barElements = data.values.map((val, i) => {
+    const barElements = props.vals.map((val, i) => {
         return (
             <Bar value={val} key={i}/>
         )
@@ -158,8 +158,8 @@ function Main(props) {
                 {barElements}
             </div>
             <form id="buttons-form">
-                <button type="button" onClick={randomize}>Randomize</button>
-                <button type="button" onClick={sort}>Sort</button>
+                <button type="button" onClick={props.randomize}>Randomize</button>
+                <button type="button" onClick={props.sort}>Sort</button>
             </form>
         </main>
     );

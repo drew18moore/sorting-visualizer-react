6fdc7624a3c2b364a13334afb7e258a60d3339b8
Drew <dcm752052@gmail.com>
2023-02-17 18:54:34 -0500

    Modified the way the header component jsx is structured. Created media query to make navbar more responsive. WIP.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,61 @@ import { useState } from "react";
 import CustomSelect from "./CustomSelect";
 import RangeSlider from "./RangeSlider";
 
-const Header = ({ setChosenAlgo, randomize, sort, canSort, setSpeed, setNumberOfBars, isRunning }) => {
+const Header = ({
+  setChosenAlgo,
+  randomize,
+  sort,
+  canSort,
+  setSpeed,
+  setNumberOfBars,
+  isRunning,
+}) => {
   return (
-    <nav>
+    <header>
       <div className="nav-logo">
         <h1>sorting-visualizer</h1>
       </div>
-      <CustomSelect onChange={(e) => setChosenAlgo(e.value)} disabled={isRunning} />
-      <RangeSlider label={"Speed"} setState={setSpeed} defaultValue={50} minValue={1} maxValue={100} disabled={isRunning} />
-      <RangeSlider label={"Size"} setState={setNumberOfBars} defaultValue={50} minValue={5} maxValue={100} disabled={isRunning} />
-      <div className="nav-btns">
-        <button className="randomize-btn" type="button" onClick={randomize} disabled={isRunning}>
-          Randomize
-        </button>
-        <button className="sort-btn" type="button" onClick={sort} disabled={!canSort}>
-          Sort
-        </button>
-      </div>
-    </nav>
+      <nav>
+        <CustomSelect
+          onChange={(e) => setChosenAlgo(e.value)}
+          disabled={isRunning}
+        />
+        <RangeSlider
+          label={"Speed"}
+          setState={setSpeed}
+          defaultValue={50}
+          minValue={1}
+          maxValue={100}
+          disabled={isRunning}
+        />
+        <RangeSlider
+          label={"Size"}
+          setState={setNumberOfBars}
+          defaultValue={50}
+          minValue={5}
+          maxValue={100}
+          disabled={isRunning}
+        />
+        <div className="nav-btns">
+          <button
+            className="randomize-btn"
+            type="button"
+            onClick={randomize}
+            disabled={isRunning}
+          >
+            Randomize
+          </button>
+          <button
+            className="sort-btn"
+            type="button"
+            onClick={sort}
+            disabled={!canSort}
+          >
+            Sort
+          </button>
+        </div>
+      </nav>
+    </header>
   );
 };
 

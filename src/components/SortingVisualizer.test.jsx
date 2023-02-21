be040c38235a97ff9facb9690a876222c51fdbbc
Drew <dcm752052@gmail.com>
2023-02-20 19:32:19 -0500

    Created a new test that checks if number of bars is updated when size slider is changed.

--- src/components/SortingVisualizer.test.jsx
+++ src/components/SortingVisualizer.test.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import SortingVisualizer from "./SortingVisualizer";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("numberOfBars", () => {
-  it("should match the default value of the size slider on initial load.", () => {
+  test("Number of rendered bars matches the default value of the size slider on initial load.", () => {
     render(<SortingVisualizer />);
 
     const sizeSlider = screen.getByTestId("size-slider");
@@ -14,4 +15,17 @@ describe("numberOfBars", () => {
 
     expect(sizeSliderValue).toEqual(barsCount);
   });
+
+  test("Number of rendered bars is properly updated when the size slider is changed", () => {
+    render(<SortingVisualizer />);
+
+    const sizeSlider = screen.getByTestId("size-slider");
+    const bars = screen.getByTestId("bars");
+    
+    fireEvent.change(sizeSlider, { target: { value: '5' } });
+    expect(bars.childElementCount).toEqual(Number(sizeSlider.closest("input").value));
+
+    fireEvent.change(sizeSlider, { target: { value: '79' } });
+    expect(bars.childElementCount).toEqual(Number(sizeSlider.closest("input").value));
+  })
 });
